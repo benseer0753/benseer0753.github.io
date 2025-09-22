6b1d95d075370cb4c08462b4d1c48d90c01ecb39
CTRL-ohs <benbobseer@gmail.com>
2025-09-22 11:31:38 -0300

    Added Themes
    Just simple dark mode and light mode buttons.

---
+++ themes.js
@@ -0,0 +1,47 @@
+function checkTheme() {
+	switch(localStorage.getItem("theme")) {
+		case "dark":
+			darkMode();
+			break;
+		case "light":
+			lightMode();
+			break;
+		default:
+			lightMode();
+			break;
+	}
+}
+
+function setDarkMode() {
+	localStorage.setItem("theme", "dark");
+	checkTheme();
+	console.log("Theme: Dark");
+}
+
+function setLightMode() {
+	localStorage.setItem("theme", "light");
+	checkTheme();
+	console.log("Theme: Light");
+}
+
+function darkMode() {
+	document.body.style.backgroundColor = "#111";
+	document.body.style.color = "white";
+	document.getElementById("mainText").style.borderLeftColor = "dimGrey";
+	const codes = document.getElementsByTagName("code");
+	//console.log(codes.length);
+	for (let i = 0; i < codes.length; ++i) {
+		codes[i].style.borderColor = "white";
+	}
+}
+
+function lightMode() {
+	document.body.style.backgroundColor = "Beige";
+	document.body.style.color = "black";
+	document.getElementById("mainText").style.borderLeftColor = "Grey";
+	const codes = document.getElementsByTagName("code");
+	console.log(codes.length);
+	for (let i = 0; i < codes.length; ++i) {
+		codes[i].style.borderColor = "black";
+	}
+}
